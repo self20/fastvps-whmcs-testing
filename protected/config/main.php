2769603bfb9f17eac60695d498a52eb0206281f9
Alexander Kondratev <caa@ipap.ru>
2013-07-29 16:19:46 +0400

    Russian project title

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Currency Rates',
+	'name'=>'Курсы валют',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
